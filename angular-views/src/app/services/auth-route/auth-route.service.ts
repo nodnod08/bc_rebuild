bf3b580cc541dafd748e0ea683df08fab8e8aa5e
Dondomie Dungca <dondomie12345@gmail.com>
2019-12-04 13:13:41 +0800

    mins

--- angular-views/src/app/services/auth-route/auth-route.service.ts
+++ angular-views/src/app/services/auth-route/auth-route.service.ts
@@ -6,50 +6,23 @@ import { MyAuthService } from './../auth/myauth.service';
   providedIn: 'root'
 })
 export class AuthRouteService implements CanActivate{
-  
-  result: Boolean
-  resultF: Boolean
 
   constructor(
     public auth: MyAuthService,
     public router: Router
   ) { }
 
-  // canActivate(): boolean {
-  //   if (!this.auth.isAuthenticated()) {
-  //     this.router.navigate(['login']);
-  //     return false;
-  //   }
-  //   return true;
-  // }
-
   async canActivate(): Promise<boolean> {
     if (!this.auth.isAuthenticated()) {
       this.router.navigate(['login']);
       return false;
-    } else {
-      // const result = await this.auth.getUserLoggedIn().subscribe()
-
-      // await result.then((data: any) => {
-      //   if(data) {
-      //     return true
-      //   }
-      //   this.router.navigate(['login']);
-      //   return false;
-      // })
-      await this.auth.getUserLoggedIn().subscribe(data => {
-        console.log(data.result)
-      })
-      
-      console.log('last run')
     }
+    if(this.auth.getUserLoggedIn()) {
+      return true
+    }
+    this.router.navigate(['login']);
+    return false;
+    
   }
 
-  // isUser(): Object {
-  //   this.auth.getUserLoggedIn().subscribe(data => {
-  //     this.result = data
-  //   })
-
-  //   return this.result
-  // }
 }
